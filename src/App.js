d193d7d664b92007edaec1f050805f44ab154909
piyushx7-cmd <piyushmishra2060@gmail.com>
2025-12-13 00:53:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -10,34 +11,39 @@ import QuizPage from "./pages/QuizPage";
 
 function App() {
   return (
-    <div className="app-root">
-      <Navbar />
-      <main className="app-main">
-        <Routes>
-          {/* Home */}
-          <Route path="/" element={<Home />} />
+    <BrowserRouter basename="/idtproject">
+      <div className="app-root">
+        <Navbar />
+        <main className="app-main">
+          <Routes>
+            {/* Home */}
+            <Route path="/" element={<Home />} />
 
-          {/* Login */}
-          <Route path="/login" element={<Login />} />
+            {/* Login */}
+            <Route path="/login" element={<Login />} />
 
-          {/* Student dashboard */}
-          <Route path="/student" element={<StudentDashboard />} />
+            {/* Student dashboard */}
+            <Route path="/student" element={<StudentDashboard />} />
 
-          {/* Student modules */}
-          <Route path="/student/modules" element={<StudentModules />} />
+            {/* Student modules */}
+            <Route path="/student/modules" element={<StudentModules />} />
 
-          {/* Individual module page */}
-          <Route path="/module/:id" element={<ModulePage />} />
+            {/* Individual module page */}
+            <Route path="/module/:id" element={<ModulePage />} />
 
-          {/* Quiz page */}
-          <Route path="/quiz/:id" element={<QuizPage />} />
+            {/* Quiz page */}
+            <Route path="/quiz/:id" element={<QuizPage />} />
 
-          {/* Fallback */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </main>
-    </div>
+            {/* Fallback */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
+
+export default App;
+
 
 export default App;
